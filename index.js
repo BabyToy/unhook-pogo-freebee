a80430a743745ccc9b565bdc5b600c187b1f4f90
Ian Bautista <victor.jackal@gmail.com>
2023-12-31 10:34:08 +0800

    fix: update exports

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const sumDeep = require('./p3');
 const excludeByProperty = require('./p2');
 const stripPrivateProperties = require('./p1');
 const applyStatusColor = require('./p4');
-// const setDefaults = require("./p6");
-// const createGreeting = require('./createGreeting');
+const setDefaults = require("./p6");
+const createGreeting = require('./p5');
 const fetchUserByNameAndUsersCompany = require("./p7");
 /**
  * Example problem with existing solution and passing test.
@@ -16,6 +16,6 @@ exports.stripPrivateProperties = (props, users) => stripPrivateProperties(props,
 exports.excludeByProperty = (prop, users) => excludeByProperty(prop, users);
 exports.sumDeep = (data) => sumDeep(data);
 exports.applyStatusColor = (colors, state) => applyStatusColor(colors, state);
-// exports.createGreeting = (phrase, name) => createGreeting(phrase, name);
-// exports.setDefaults = (object) => setDefaults(object);
+exports.createGreeting = (phrase, name) => createGreeting(phrase, name);
+exports.setDefaults = (object) => setDefaults(object);
 exports.fetchUserByNameAndUsersCompany = (user, services) => fetchUserByNameAndUsersCompany(user, services);
